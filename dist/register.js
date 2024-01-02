19704442ae8fb0a2d623dbdf51a54945392e6eb3
Florian Dendorfer <florian.dendorfer@jls.ch>
2024-01-02 10:23:57 +0100

    1.1.0

--- dist/register.js
+++ dist/register.js
@@ -5,24 +5,17 @@ import React from 'react';
 
 // src/register.tsx
 function CodePanel({ sourceFiles }) {
-  if (sourceFiles.length === 0)
-    return /* @__PURE__ */ React.createElement("div", { style: { padding: "1rem" } }, "No source code available");
-  return /* @__PURE__ */ React.createElement(TabsState, null, sourceFiles.map((file, i) => /* @__PURE__ */ React.createElement(
-    "div",
-    {
-      key: i,
-      id: file?.id || i.toString(),
-      title: file?.name || i.toString()
-    },
-    /* @__PURE__ */ React.createElement(
+  return /* @__PURE__ */ React.createElement(TabsState, null, sourceFiles.map((file, i) => (
+    // id is used as the key and title as the tab title by TabsState
+    /* @__PURE__ */ React.createElement("div", { key: i, id: i.toString(), title: file.name || i.toString() }, /* @__PURE__ */ React.createElement(
       SyntaxHighlighter,
       {
-        language: file?.language || "typescript",
         copyable: true,
+        language: file.language || "typescript",
         showLineNumbers: true
       },
-      file?.code
-    )
+      file.code
+    ))
   )));
 }
 
@@ -32,7 +25,16 @@ var PANEL_ID = `${ADDON_ID}/panel`;
 addons.register(ADDON_ID, () => {
   const render = ({ active = false }) => {
     const sourceFiles = useParameter("sourceCode", []);
-    return /* @__PURE__ */ React.createElement(AddonPanel, { active }, /* @__PURE__ */ React.createElement(CodePanel, { sourceFiles }));
+    const preparedFiles = Array.isArray(sourceFiles) ? sourceFiles.map((file) => {
+      if (typeof file === "string")
+        return { code: file };
+      if (typeof file === "object" && file.code)
+        return file;
+      return null;
+    }).filter((file) => !!file) : [];
+    if (preparedFiles.length === 0)
+      return /* @__PURE__ */ React.createElement("div", { style: { padding: "1rem" } }, "No source code available");
+    return /* @__PURE__ */ React.createElement(AddonPanel, { active }, /* @__PURE__ */ React.createElement(CodePanel, { sourceFiles: preparedFiles }));
   };
   addons.add(PANEL_ID, {
     type: types.PANEL,
